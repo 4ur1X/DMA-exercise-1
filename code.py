71944939d750a2876edca9f65c247f6f2e5d0b98
4ur1X <ronit.singh@nyu.edu>
2023-09-17 16:26:07 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,11 +1,9 @@
-# SPDX-FileCopyrightText: 2018 Kattni Rembor for Adafruit Industries
-#
-# SPDX-License-Identifier: MIT
+# On-board LED Homework for Desert Media Art
+# Ronit Singh
+# 2023-09-17
 
-"""CircuitPython Essentials Internal RGB LED red, green, blue example"""
 import time
 import board
-from rainbowio import colorwheel
 
 if hasattr(board, "APA102_SCK"):
     import adafruit_dotstar
@@ -18,25 +16,86 @@ else:
 
 led.brightness = 1
 
+# changing LED colors based on country flags
 while True:
+    # india flag
+    led[0] = (255,69,0) # orange
+    time.sleep(1)
+    led[0] = (0,0,255) # blue
+    time.sleep(1)
+    led[0] = (0, 128, 0) # green
+    time.sleep(1)
+
+    # showing no color to separate next flag
+    led[0] = (0, 0, 0)
+    time.sleep(1)
+
+    # uae flag
+    led[0] = (255, 0, 0) # red
+    time.sleep(1)
+    led[0] = (0, 128, 0) # green
+    time.sleep(1)
+    led[0] = (255,255,255) # white
+    time.sleep(1)
+
+    # showing no color to separate next flag
+    led[0] = (0, 0, 0)
+    time.sleep(1)
+
+    #australia flag
+    led[0] = (255, 0, 0) # red
+    time.sleep(1)
+    led[0] = (0,0,255) # blue
+    time.sleep(1)
+    led[0] = (255,255,255) # white
+    time.sleep(1)
+
+    # showing no color to separate next flag
+    led[0] = (0, 0, 0)
+    time.sleep(1)
+
+    #singapore flag
+    led[0] = (255, 0, 0) # red
+    time.sleep(1)
+    led[0] = (255,255,255) # white
+    time.sleep(1)
+
+    # showing no color to separate next flag
+    led[0] = (0, 0, 0)
+    time.sleep(1)
+
+    #united states flag
+    led[0] = (255, 0, 0) # red
+    time.sleep(1)
+    led[0] = (0,0,255) # blue
+    time.sleep(1)
+    led[0] = (255,255,255) # white
+    time.sleep(1)
+
+    # showing no color to separate next flag
+    led[0] = (0, 0, 0)
+    time.sleep(3)
+
+    #----------time variations-------------
+
     #india flag
     led[0] = (255,69,0)
     time.sleep(1)
     led[0] = (0,0,255)
-    time.sleep(1)
+    time.sleep(0.25)
     led[0] = (0, 128, 0)
-    time.sleep(1)
+    time.sleep(0.25)
 
     led[0] = (0, 0, 0)
-    time.sleep(1)
+    time.sleep(0.25)
 
     #uae flag
     led[0] = (255, 0, 0)
     time.sleep(1)
     led[0] = (0, 128, 0)
-    time.sleep(1)
+    time.sleep(0.25)
     led[0] = (255,255,255)
-    time.sleep(1)
+    time.sleep(0.25)
 
     led[0] = (0, 0, 0)
     time.sleep(1)
@@ -45,61 +104,6 @@ while True:
     led[0] = (255, 0, 0)
     time.sleep(1)
     led[0] = (0,0,255)
-    time.sleep(1)
-    led[0] = (255,255,255)
-    time.sleep(1)
-
-    led[0] = (0, 0, 0)
-    time.sleep(1)
-
-    #singapore flag
-    led[0] = (255, 0, 0)
-    time.sleep(1)
-    led[0] = (255,255,255)
-    time.sleep(1)
-
-    led[0] = (0, 0, 0)
-    time.sleep(1)
-
-    #united states flag
-    led[0] = (255, 0, 0)
-    time.sleep(1)
-    led[0] = (0,0,255)
-    time.sleep(1)
-    led[0] = (255,255,255)
-    time.sleep(1)
-
-    led[0] = (0, 0, 0)
-    time.sleep(3)
-
-    #-----------------------
-
-    #india flag
-    led[0] = (255,69,0)
-    time.sleep(1)
-    led[0] = (0,0,255)
-    time.sleep(0.25)
-    led[0] = (0, 128, 0)
-    time.sleep(0.25)
-
-    led[0] = (0, 0, 0)
-    time.sleep(0.25)
-
-    #uae flag
-    led[0] = (255, 0, 0)
-    time.sleep(1)
-    led[0] = (0, 128, 0)
-    time.sleep(0.25)
-    led[0] = (255,255,255)
-    time.sleep(0.25)
-
-    led[0] = (0, 0, 0)
-    time.sleep(1)
-
-    #australia flag
-    led[0] = (255, 0, 0)
-    time.sleep(1)
-    led[0] = (0,0,255)
     time.sleep(0.25)
     led[0] = (255,255,255)
     time.sleep(0.25)
@@ -127,7 +131,7 @@ while True:
     led[0] = (0, 0, 0)
     time.sleep(3)
 
-    #-------------------
+    #----------time variations-------------
 
     #india flag
     led[0] = (255,69,0)
@@ -201,9 +205,3 @@ while True:
 
     led[0] = (0,0,0)
     time.sleep(0.5)
-
-
-
-
-
-
